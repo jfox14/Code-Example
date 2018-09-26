2ee8183012b8be9e60646dc6eefd41b1952cc91d
Jonathan Fox <jonathan.fox@7signal.com>
2018-09-26 02:45:09 -0400

    updates

--- src/components/contactFormContainer.js
+++ src/components/contactFormContainer.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
-import { withRouter } from 'react-router'
 import api from '../services/api'
 
 function withContactFormStateAndHandlers (ComponentToWrap) {
